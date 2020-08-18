317d12f4a249a5ed5b817d6c78bde3138e6041ff
mike <myke_roly@hotmail.com>
2020-08-18 02:40:24 -0300

    change port 3030 to 8080

--- server/dist/js/app.js
+++ server/dist/js/app.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.status(201).json({ message: "Hi!" });
 });
