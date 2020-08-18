52c2df291cc2de9d6b9e3754e28d7137f2faff1c
mike <myke_roly@hotmail.com>
2020-08-18 02:31:46 -0300

    change port

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,9 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Response } from 'express';
 
 const app: Express = express();
-const PORT: string | number = process.env.PORT || 3030;
+const PORT: string | number = process.env.PORT || 8080;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_, res: Response) => {
   res.status(201).json({ message: "Hi!" });
 });
 
