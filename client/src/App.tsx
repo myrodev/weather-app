676ed62ab5f570e76b057f7eb278b2b871075826
mike <myke_roly@hotmail.com>
2020-08-18 01:22:06 -0300

    chage styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
 import Card from './components/card';
-import Button from './components/button';
 
 export interface userI {
   name: {
-    first: string,
-    last: string,
-    title: string
-  },
+    first: string;
+    last: string;
+    title: string;
+  };
   id: {
-    value: string
-  },
+    value: string;
+  };
   picture: {
-    large: string
-  },
-  gender: string
+    large: string;
+  };
+  gender: string;
 }
 
 const App: React.SFC = () => {
   const url: string = 'https://randomuser.me/api/';
-  const [state, setState] = useState< userI[] | undefined>([]);
+  const [state, setState] = useState<userI[] | undefined>([]);
 
   React.useEffect(() => {
     const ApiReq = async (): Promise<any> => {
@@ -28,7 +27,7 @@ const App: React.SFC = () => {
       const data = await response.json();
 
       setState(data.results);
-    }
+    };
 
     ApiReq();
   }, []);
@@ -36,12 +35,11 @@ const App: React.SFC = () => {
   return (
     <div className="App">
       <h1>User Random Api</h1>
-      { state && state.map((user) => (
-       <Card key={user.id.value} user={user} />
-      ))}
-      <Button text="Change" />
+      <section className="Cards">
+        {state && state.map((user) => <Card key={user.id.value} user={user} />)}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
